620a655d30647079afff7521dbd5cd92809244ba
Than Ke <nkthan@tma.com.vn>
2018-06-25 22:44:50 +0700

    SpringBootTest with a Real Web Server

--- Product/src/test/java/org/product/controller/LocationControllerTests.java
+++ Product/src/test/java/org/product/controller/LocationControllerTests.java
@@ -14,41 +14,68 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.product.model.Location;
+import org.product.repository.LocationRepository;
 import org.product.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+//Testing with a running server
 
-
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LocationControllerTests {
 	
-	@Mock
-	private LocationService locationService;
 	
-	@InjectMocks
-	private LocationController locationController;
+	@LocalServerPort
+	private int port;
 	
-	private MockMvc mockMvc;
+//	@Autowired
+//	private WebTestClient webClient;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
 	
 	
+	@MockBean
+	private LocationRepository locationRepository;
+	
+	
+	
+//	@Mock
+//	private LocationService locationService;
+//	
+//	@InjectMocks
+//	private LocationController locationController;
+	
+//	private MockMvc mockMvc;
+	
+	private String host="http://localhost:"+port;
 	@Before
 	void setUp() {
-	    mockMvc = MockMvcBuilders.standaloneSetup(this.locationController).build();
+	
+	    //mockMvc = MockMvcBuilders.standaloneSetup(this.locationController).build();
 	}
 	
 	@Test
 	public void testShowAllLocation() throws Exception {
-		List<Location> listLocation=Arrays.asList(
-				new Location(UUID.randomUUID(),"Ha Noi","Viet Nam"),
-				new Location(UUID.randomUUID(),"Binh Dinh","Viet Nam")
-				);
-		   MvcResult mvcResult = mockMvc.perform(get("/api/locations")).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
-
-		    Assert.assertEquals("failure", mvcResult.getResponse(), listLocation);
+//		List<Location> listLocation=Arrays.asList(
+//				new Location(UUID.randomUUID(),"Ha Noi","Viet Nam"),
+//				new Location(UUID.randomUUID(),"Binh Dinh","Viet Nam")
+//				);
+//		   MvcResult mvcResult = mockMvc.perform(get("/api/locations")).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+//
+//		    Assert.assertEquals("failure", mvcResult.getResponse(), listLocation);
+		given(this.locationRepository.)
 	}
 
 }
